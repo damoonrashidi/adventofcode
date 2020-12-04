a0ad8dc27f2a769d3fba41246f546eef571c73c3
Damoon Rashidi <damoon.rashidi@gmail.com>
2020-12-04 15:31:03 +0100

    Day3 puzzle 2

--- 2020/day3.ts
+++ 2020/day3.ts
@@ -1,15 +1,31 @@
 import { input } from './input/day3-input';
 
-const w = input[0].length;
-let x = 0;
-let trees = 0;
+const puzzleOne = (rows: string[], stepX = 3, stepY = 1): number => {
+  const w = rows[0].length;
+  let x = 0;
+  let trees = 0;
 
-for (let y = 0; y < input.length; y++) {
-  const charAtSpace = input[y].charAt(x);
-  if (charAtSpace === '#') {
-    trees++;
+  for (let y = 0; y < rows.length; y += stepY) {
+    if (!rows[y]) {
+      return trees;
+    }
+
+    const charAtSpace = rows[y].charAt(x);
+    if (charAtSpace === '#') {
+      trees++;
+    }
+    x = (x + stepX) % w;
   }
-  x = (x + 3) % w;
-}
 
-console.log(trees);
+  return trees;
+};
+
+const puzzleTwo = (rows: string[]): number => {
+  return [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]].reduce(
+    (product, [stepX, stepY]) => product * puzzleOne(rows, stepX, stepY),
+    1
+  );
+};
+
+console.log('Puzzle one: ', puzzleOne(input));
+console.log('Puzzle two: ', puzzleTwo(input));
