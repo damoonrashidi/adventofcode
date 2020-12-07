359e3dfd453b432e95406d6d74bb8f21e62e1543
Damoon Rashidi <damoon.rashidi@gmail.com>
2020-12-07 15:30:52 +0100

    Day 7 puzzle: 1

--- 2020/day7.ts
+++ 2020/day7.ts
@@ -1,7 +1,22 @@
-import { testInput, input } from './input/day7-input';
+import { Bag, input } from './input/day7-input';
 
-function puzzleOne(_: any): any {}
+const canContainGoldBag = (contains: string[], bags: Bag[]): boolean => {
+  if (contains.length === 0) {
+    return false;
+  } else if (contains.includes('shiny gold')) {
+    return true;
+  }
+
+  return contains.reduce((truth: boolean, color: string) => {
+    const bag = bags.find(s => s.color === color) as Bag;
+    return truth || canContainGoldBag(bag.contains, bags);
+  }, false);
+};
+
+function puzzleOne(bags: Bag[]): number {
+  return bags.filter(bag => canContainGoldBag(bag.contains, bags)).length;
+}
 function puzzleTwo(_: any): any {}
 
 console.log('Puzzle one:', puzzleOne(input));
-console.log('Puzzle two:', puzzleTwo(testInput));
+console.log('Puzzle two:', puzzleTwo(input));
---
+++ 2020/day8.ts
@@ -0,0 +1,7 @@
+import { testInput, input } from './input/day7-input';
+
+function puzzleOne(_: any): any {}
+function puzzleTwo(_: any): any {}
+
+console.log('Puzzle one:', puzzleOne(testInput));
+console.log('Puzzle two:', puzzleTwo(input));
