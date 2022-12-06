09d4d228119dffc41c4d09feb6d18a5a463128e0
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-12-06 22:05:29 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Advent of Code 2022
+
+Solution for the first puzzle for every day (so far).
+
+## How to run
+
+```bash
+cargo run --bin {dayX} -- ./src/inputs/day3.txt
+```
+
+The few tests that are available can be run with
+
+```bash
+cargo test
+```
--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file = fs::read_to_string(&args[1]).unwrap();
+
+    println!("{}", puzzle_one(file));
+}
+
+fn puzzle_one(input: String) -> String {
+    input
+}
