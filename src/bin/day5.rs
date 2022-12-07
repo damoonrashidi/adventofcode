d3acb977ca8a96497e92366765f6344ff1127cd3
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-12-07 22:11:11 +0100

    Day5

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -6,26 +6,74 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = fs::read_to_string(&args[1]).unwrap();
 
-    println!("{}", puzzle_one(file));
+    println!("{}", puzzle_one(file.clone()));
+    println!("{}", puzzle_two(file.clone()));
 }
 
 fn puzzle_one(input: String) -> String {
     let (state_str, ops) = input.split_once("\n\n").unwrap();
 
-    let state = parse_state(state_str);
-
-    println!("{:?}", state);
+    let mut state = parse_state(state_str);
 
     ops.lines().into_iter().for_each(|line| {
         let reg = Regex::new(r#"move (\d+) from (\d+) to (\d+)"#).unwrap();
         let caps = reg.captures(line).unwrap();
+
+        let amount = &caps[1].parse::<usize>().unwrap();
+        let from_index = &caps[2].parse::<usize>().unwrap() - 1;
+        let to_index = &caps[3].parse::<usize>().unwrap() - 1;
+
+        let (moving, from_after) = state.get(&from_index).unwrap().split_at(*amount);
+        let to_after = format!(
+            "{}{}",
+            moving.chars().rev().collect::<String>(),
+            state.get(&to_index).unwrap(),
+        );
+
+        state.insert(from_index, from_after.into());
+        state.insert(to_index, to_after.into());
     });
 
-    "".into()
+    let mut ans = String::from("");
+    for i in 0..state.len() {
+        let value = state.get(&i).unwrap();
+        let c = value.chars().next().unwrap();
+        ans = format!("{}{}", ans, c);
+    }
+    ans
 }
 
-fn parse_state(input: &str) -> HashMap<usize, Vec<char>> {
-    let mut map: HashMap<usize, Vec<char>> = HashMap::new();
+fn puzzle_two(input: String) -> String {
+    let (state_str, ops) = input.split_once("\n\n").unwrap();
+
+    let mut state = parse_state(state_str);
+
+    ops.lines().into_iter().for_each(|line| {
+        let reg = Regex::new(r#"move (\d+) from (\d+) to (\d+)"#).unwrap();
+        let caps = reg.captures(line).unwrap();
+
+        let amount = &caps[1].parse::<usize>().unwrap();
+        let from_index = &caps[2].parse::<usize>().unwrap() - 1;
+        let to_index = &caps[3].parse::<usize>().unwrap() - 1;
+
+        let (moving, from_after) = state.get(&from_index).unwrap().split_at(*amount);
+        let to_after = format!("{}{}", moving, state.get(&to_index).unwrap(),);
+
+        state.insert(from_index, from_after.into());
+        state.insert(to_index, to_after.into());
+    });
+
+    let mut ans = String::from("");
+    for i in 0..state.len() {
+        let value = state.get(&i).unwrap();
+        let c = value.chars().next().unwrap();
+        ans = format!("{}{}", ans, c);
+    }
+    ans
+}
+
+fn parse_state(input: &str) -> HashMap<usize, String> {
+    let mut map: HashMap<usize, String> = HashMap::new();
 
     input
         .as_bytes()
@@ -33,16 +81,18 @@ fn parse_state(input: &str) -> HashMap<usize, Vec<char>> {
         .into_iter()
         .enumerate()
         .for_each(|(index, letter)| {
-            println!("{}{:?}", , letter[1] as char);
-            let column = index % 3;
+            let column = index % 9;
 
-            if map.contains_key(&column) && letter[1] as char != ' ' {
-                let v = map.get_mut(&column).unwrap();
-                v.insert(index, letter[1] as char);
+            if letter[1] as char == ' ' || (b'0'..b'9').contains(&letter[1]) {
+                return;
             }
-        });
 
-    println!("{:?}", map);
+            if let Some(s) = map.get(&column) {
+                map.insert(column, format!("{}{}", s, letter[1] as char));
+            } else {
+                map.insert(column, format!("{}", letter[1] as char));
+            };
+        });
 
     map
 }
