f6c75cdf13275d53a8ade9f4ff766674448171f5
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-12-08 16:53:23 +0100

    d3p2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -4,10 +4,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = fs::read_to_string(&args[1]).unwrap();
 
-    println!("{}", puzzle_one(file));
+    println!("{}", puzzle_one(&file));
+    println!("{}", puzzle_two(&file));
 }
 
-fn puzzle_one(input: String) -> u32 {
+fn puzzle_one(input: &String) -> u32 {
     input
         .lines()
         .map(|line| line.split_at(line.len() / 2))
@@ -16,6 +17,28 @@ fn puzzle_one(input: String) -> u32 {
             let b: HashSet<u8> = second.bytes().into_iter().collect();
 
             a.intersection(&b).map(|item| prio(*item)).sum()
+        })
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum()
+}
+
+fn puzzle_two(input: &String) -> u32 {
+    input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(|lines| {
+            let a: HashSet<u8> = lines[0].bytes().into_iter().collect();
+            let b: HashSet<u8> = lines[1].bytes().into_iter().collect();
+            let c: HashSet<u8> = lines[2].bytes().into_iter().collect();
+
+            a.intersection(&b)
+                .cloned()
+                .collect::<HashSet<u8>>()
+                .intersection(&c)
+                .map(|item| prio(*item))
+                .sum()
         })
         .collect::<Vec<u32>>()
         .iter()
