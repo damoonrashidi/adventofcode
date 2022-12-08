8fe9e84e99c47e01849f1a771daabf86e197b9c4
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-12-08 16:34:24 +0100

    day4p2

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -5,10 +5,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = fs::read_to_string(&args[1]).unwrap();
 
-    println!("{}", day1(file));
+    println!("{}", puzzle_one(&file));
+    println!("{}", puzzle_two(&file));
 }
 
-fn day1(input: String) -> usize {
+fn puzzle_one(input: &String) -> usize {
     input
         .lines()
         .map(|line| {
@@ -19,6 +20,23 @@ fn day1(input: String) -> usize {
 
             (first.start <= second.start && first.end >= second.end)
                 || second.start <= first.start && second.end >= first.end
+        })
+        .filter(|l| *l)
+        .collect::<Vec<bool>>()
+        .len()
+}
+
+fn puzzle_two(input: &String) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let re = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
+            let caps = re.captures(line).unwrap();
+            let first = &caps[1].parse::<u32>().unwrap()..&caps[2].parse().unwrap();
+            let second = &caps[3].parse::<u32>().unwrap()..&caps[4].parse().unwrap();
+
+            (first.start <= second.start && first.end >= second.start)
+                || (second.start <= first.start && second.end >= first.start)
         })
         .filter(|l| *l)
         .collect::<Vec<bool>>()
