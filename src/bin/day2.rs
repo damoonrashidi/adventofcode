8f6d3cc3dbeb39189dcbc2c245cab54e6c938258
Damoon Rashidi <damoon.rashidi@gmail.com>
2022-12-09 18:03:42 +0100

    Reconcile

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -4,7 +4,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = fs::read_to_string(&args[1]).unwrap();
 
-    println!("{}", puzzle_one(file));
+    println!("{}", puzzle_one(file.clone()));
+    println!("{}", puzzle_two(file));
 }
 
 fn puzzle_one(input: String) -> u32 {
@@ -22,6 +23,21 @@ fn puzzle_one(input: String) -> u32 {
         .sum()
 }
 
+fn puzzle_two(input: String) -> u32 {
+    let moves = input
+        .lines()
+        .map(|line| {
+            let split = line.chars().collect::<Vec<char>>();
+            (split[0], split[2])
+        })
+        .collect::<Vec<(char, char)>>();
+
+    moves
+        .into_iter()
+        .map(|(op_move, me_move)| -> u32 { should_pick(op_move, me_move) })
+        .sum()
+}
+
 fn outcome(op_move: char, me_move: char) -> u32 {
     match (op_move, me_move) {
         ('A', 'X') => 3, //rock -> rock
@@ -33,6 +49,21 @@ fn outcome(op_move: char, me_move: char) -> u32 {
         ('C', 'X') => 6, //scissor -> rock
         ('C', 'Y') => 0, //scissor -> paper
         ('C', 'Z') => 3, //scissor -> scissor
+        _ => unreachable!(),
+    }
+}
+
+fn should_pick(op_move: char, me_move: char) -> u32 {
+    match (op_move, me_move) {
+        ('A', 'X') => 0 + move_score('Z'), //rock -> lose
+        ('A', 'Y') => 3 + move_score('X'), //rock -> draw
+        ('A', 'Z') => 6 + move_score('Y'), //rock -> win
+        ('B', 'X') => 0 + move_score('X'), //paper -> lose
+        ('B', 'Y') => 3 + move_score('Y'), //paper -> draw
+        ('B', 'Z') => 6 + move_score('Z'), //paper -> win
+        ('C', 'X') => 0 + move_score('Y'), //scissor -> lose
+        ('C', 'Y') => 3 + move_score('Z'), //scissor -> draw
+        ('C', 'Z') => 6 + move_score('X'), //scissor -> win
         _ => unreachable!(),
     }
 }
