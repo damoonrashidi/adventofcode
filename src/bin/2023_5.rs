b22d0e2ab15ff99f98e606aa90ee66cf0e000350
Damoon Rashidi <damoon.rashidi@gmail.com>
2023-12-05 14:02:17 +0100

    2023 Day 5 part 1

---
+++ src/bin/2023_5.rs
@@ -0,0 +1,121 @@
+use std::ops::Range;
+
+fn main() {
+    let input = include_str!("../inputs/2023_5.txt").trim();
+
+    println!("puzzle one: {}", puzzle_one(input));
+    println!("puzzle two: {}", puzzle_two(input));
+}
+
+fn puzzle_one(input: &str) -> usize {
+    let (first_line, _) = input.split_once('\n').unwrap();
+    let seeds: Vec<usize> = first_line
+        .strip_prefix("seeds: ")
+        .unwrap()
+        .split_whitespace()
+        .map(|seed| seed.parse().unwrap())
+        .collect();
+
+    let map_tunnel = parse_maps(input);
+
+    let mut outputs: Vec<usize> = vec![];
+
+    for seed in seeds {
+        let mut ins = seed;
+        for ranges in &map_tunnel {
+            ins = if let Some((to, from)) = ranges.iter().find(|(_, from)| from.contains(&ins)) {
+                map(ins, from.clone(), to.clone())
+            } else {
+                ins
+            };
+        }
+
+        outputs.push(ins);
+    }
+
+    outputs
+        .into_iter()
+        .min()
+        .expect("could not find any location")
+}
+
+fn puzzle_two(input: &str) -> usize {
+    input.len()
+}
+
+fn parse_maps(input: &str) -> Vec<Vec<(Range<usize>, Range<usize>)>> {
+    input
+        .split("\n\n")
+        .skip(1)
+        .map(|chunk| {
+            chunk
+                .lines()
+                .skip(1)
+                .map(|line| {
+                    let a = line
+                        .split_whitespace()
+                        .map(|str| str.parse().unwrap())
+                        .collect::<Vec<usize>>();
+
+                    let to_start = a[0];
+                    let from_start = a[1];
+                    let len = a[2];
+
+                    (to_start..to_start + len, from_start..from_start + len)
+                })
+                .collect()
+        })
+        .collect()
+}
+
+#[inline]
+#[must_use]
+pub fn map(value: usize, from: Range<usize>, to: Range<usize>) -> usize {
+    let diff = from.start.abs_diff(value);
+    to.start + diff
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle_one;
+
+    #[test]
+    fn test() {
+        let actual = puzzle_one(
+            r"seeds: 79 14 55 13
+
+        seed-to-soil map:
+        50 98 2
+        52 50 48
+
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
+
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
+
+        water-to-light map:
+        88 18 7
+        18 25 70
+
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
+
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
+
+        humidity-to-location map:
+        60 56 37
+        56 93 4",
+        );
+        assert_eq!(actual, 35);
+    }
+}
