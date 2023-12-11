57019c864b58bb80e435c36bc59bb11ca1bcdd64
Damoon Rashidi <damoon.rashidi@gmail.com>
2023-12-11 19:28:44 +0100

    2023 Day 11 Part 2

--- src/bin/2023_11.rs
+++ src/bin/2023_11.rs
@@ -1,64 +1,5 @@
 use std::collections::HashSet;
 
-fn main() {
-    let universe = expand_universe(include_str!("../../inputs/2023/11.txt").trim());
-    let input = parse(&universe);
-
-    println!("puzzle one: {}", puzzle_one(&input));
-    println!("puzzle two: {}", puzzle_two(&input));
-}
-
-fn puzzle_one(galaxies: &Vec<Coord>) -> usize {
-    let mut unique_pairs = HashSet::new();
-
-    for i in 0..galaxies.len() {
-        for j in i + 1..galaxies.len() {
-            unique_pairs.insert((galaxies[i], galaxies[j]));
-        }
-    }
-
-    unique_pairs.iter().map(|(a, b)| a.distance(*b)).sum()
-}
-
-fn puzzle_two(input: &Vec<Coord>) -> usize {
-    input.len()
-}
-
-fn parse(map: &[Vec<char>]) -> Vec<Coord> {
-    let mut coords = vec![];
-
-    for y in 0..map.len() {
-        for x in 0..map[0].len() {
-            if map[y][x] == '#' {
-                coords.push(Coord(x, y));
-            }
-        }
-    }
-
-    coords
-}
-
-fn expand_universe(input: &str) -> Vec<Vec<char>> {
-    let mut map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
-
-    for y in (0..map.len()).rev() {
-        if map[y].clone().into_iter().all(|c| c == '.') {
-            map.insert(y + 1, map[y].clone());
-        }
-    }
-
-    for x in (0..map[0].len()).rev() {
-        if map.iter().map(|r| r[x]).all(|c| c == '.') {
-            #[allow(clippy::needless_range_loop)]
-            for xs in 0..map.len() {
-                map[xs].insert(x + 1, '.');
-            }
-        }
-    }
-
-    map
-}
-
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 struct Coord(usize, usize);
 
@@ -69,26 +10,62 @@ impl Coord {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use crate::{expand_universe, parse, puzzle_one};
+fn main() {
+    let input = include_str!("../../inputs/2023/11.txt").trim();
 
-    #[test]
-    fn test_puzzle_one() {
-        let universe = expand_universe(
-            r"...#......
-.......#..
-#.........
-..........
-......#...
-.#........
-.........#
-..........
-.......#..
-#...#.....",
-        );
-        let map = parse(&universe);
-        let actual = puzzle_one(&map);
-        assert_eq!(actual, 374);
+    println!("puzzle one: {}", puzzle(input, 2));
+    println!("puzzle two: {}", puzzle(input, 1_000_000));
+}
+
+fn puzzle(input: &str, multiplier: usize) -> usize {
+    let (galaxies, ys, xs) = parse(input);
+
+    let mut unique_pairs = HashSet::new();
+
+    for i in 0..galaxies.len() {
+        for j in i + 1..galaxies.len() {
+            unique_pairs.insert((galaxies[i], galaxies[j]));
+        }
     }
+
+    unique_pairs
+        .iter()
+        .map(|(a, b)| {
+            let x_r = a.0.min(b.0)..b.0.max(a.0);
+            let y_r = a.1.min(b.1)..b.1.max(a.1);
+            let crossed_x = xs.iter().filter(|x| x_r.contains(x)).count();
+            let crossed_y = ys.iter().filter(|y| y_r.contains(y)).count();
+
+            a.distance(*b) + (crossed_x + crossed_y) * (multiplier - 1)
+        })
+        .sum()
+}
+
+fn parse(input: &str) -> (Vec<Coord>, Vec<usize>, Vec<usize>) {
+    let map: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    let mut ys = vec![];
+    (0..map.len()).for_each(|y| {
+        if map[y].clone().iter().all(|c| c == &'.') {
+            ys.push(y);
+        }
+    });
+
+    let mut xs = vec![];
+    for x in 0..map[0].len() {
+        if map.iter().map(|r| r[x]).all(|c| c == '.') {
+            xs.push(x);
+        }
+    }
+
+    let mut galaxies: Vec<Coord> = vec![];
+    for y in 0..map.len() {
+        for x in 0..map[0].len() {
+            if map[y][x] == '#' {
+                galaxies.push(Coord(x, y));
+            }
+        }
+    }
+
+    (galaxies, ys, xs)
 }
