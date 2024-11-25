6f707994027dbf8f0e641343420e4d753044eecb
Damoon Rashidi <damoon.rashidi@gmail.com>
2024-11-25 20:07:52 +0100

    2016/3

--- src/bin/2016_3.rs
+++ src/bin/2016_3.rs
@@ -21,7 +21,32 @@ fn puzzle_one(input: &str) -> usize {
 }
 
 fn puzzle_two(input: &str) -> usize {
-    input.len()
+    let mut count = 0;
+    let values = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|w| w.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<Vec<_>>>();
+
+    for x in 0..3 {
+        for y in (0..values.len()).step_by(3) {
+            let a = values[y][x];
+            let b = values[y + 1][x];
+            let c = values[y + 2][x];
+
+            let mut sorted = [a, b, c];
+            sorted.sort_unstable();
+
+            if sorted[0] + sorted[1] > sorted[2] {
+                count += 1;
+            }
+        }
+    }
+
+    count
 }
 
 #[cfg(test)]
